8b1263fc36af394a84550aeed46b39e3064e8187
Brijesh <softbrijeshio@gmail.com>
2025-11-17 20:11:43 +0530

    chore: setup tailwindcss + shadcn for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
 import "@/App.css";
 
 function App() {
   return (
     <>
-      <h1>Welcome to My Portfolio</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="mb-2">Welcome to My Portfolio</h1>
+        <Button>Get Started</Button>
+      </ThemeProvider>
     </>
   );
 }
