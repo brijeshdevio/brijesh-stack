30204f5f5ec01bedcdbbba440a47a73b8dabdafd
Brijesh <softbrijeshio@gmail.com>
2025-11-17 20:28:56 +0530

    feat: add navbar and change shadcn theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { Button } from "@/components/ui/button";
+import { Navbar } from "@/components/Navbar";
 import "@/App.css";
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <h1 className="mb-2">Welcome to My Portfolio</h1>
-        <Button>Get Started</Button>
+        <Navbar />
       </ThemeProvider>
     </>
   );
